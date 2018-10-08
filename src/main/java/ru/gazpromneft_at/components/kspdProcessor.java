92391f2f30f2d95b2b3b032fa9814479b0c258ca
Alexander S <Sokolov.AAlek@gazprom-neft.ru>
2018-10-08 16:11:06 +0300

    changed libraries

--- src/main/java/ru/gazpromneft_at/components/kspdProcessor.java
+++ src/main/java/ru/gazpromneft_at/components/kspdProcessor.java
@@ -2,14 +2,19 @@ package ru.gazpromneft_at.components;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import ru.gazpromneft_at.model.Messaga;
 
-public class kspdProcessor implements Processor {
+public class KspdProcessor implements Processor {
+    private Logger logger = LogManager.getLogger(MessageReceiver.class);
     public void process(Exchange exchange) throws Exception {
         String correlation = (String) exchange.getMessage().getHeader("JMSCorrelationID");
-        exchange.getOut().setBody(new Messaga(345L, "Proceseed with KSPD!"));
-        exchange.getOut().setHeader("JMSCorrelationID", correlation);
-        exchange.getOut().setHeader("Accept", "Application/json");
-        exchange.getOut().setHeader("Content-Type", "application/json; charset=UTF-8");
+        logger.info("KSPD LOGICS with correlId: " + correlation);
+        exchange.getIn().setBody(new Messaga(345L, "Proceseed with KSPD!", correlation));
+        exchange.getIn().setHeader("JMSCorrelationID", correlation);
+        exchange.getIn().setHeader("Accept", "Application/json");
+        exchange.getIn().setHeader("Content-Type", "application/json; charset=UTF-8");
+        exchange.getIn().setHeader("JMSCorrelationID", correlation);
     }
 }
